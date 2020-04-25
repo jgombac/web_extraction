3e069589b421c27b272ee7df6d4e2442f098de38
Rok <rp8257@student.uni-lj.si>
2020-04-25 18:24:53 +0200

    Added wrapper file and command line arg.

--- run-extraction.py
+++ run-extraction.py
@@ -1,10 +1,16 @@
 import sys
 import xpath_extract
+import wrapper
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         arg = sys.argv[1]
         if arg == "A":
             xpath_extract.run_all()
+        elif arg == "B":
+            pass
+        else:
+            wrapper.run()
+
     else:
         xpath_extract.run_all()
